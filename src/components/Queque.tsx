08c1284c0546eb33e06ec82f3f2388339189a878
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-10-14 16:46:26 +0300

    add: şu anlık boşluğu upgrade gibi bir kutuyla tamamladım, amacım tasarım yoğunluğunu görmek

--- src/components/Queque.tsx
+++ src/components/Queque.tsx
@@ -1,9 +1,13 @@
 import MusicList from "./musics/MusicList";
+import UpgradePremium from "./UpgradePremium";
 
 const Queque = () => {
   return (
-    <div className="w-160 p-5">
-      <MusicList />
+    <div className="w-160 p-5 flex flex-col justify-between h-full">
+      <div className="flex flex-col gap-8">
+        <MusicList />
+        <UpgradePremium />
+      </div>
     </div>
   );
 };
---
+++ src/components/UpgradePremium.tsx
@@ -0,0 +1,19 @@
+import { FaArrowUp } from "react-icons/fa";
+const UpgradePremium = () => {
+  return (
+    <div className="flex items-center justify-between p-4 bg-[#2c1314] rounded-2xl">
+      <div className="flex flex-col gap-1">
+        <h3 className="font-semibold">Upgrade To Premium</h3>
+        <p className="text-xs">
+          Upgrade to premium plan to get premium & insights
+        </p>
+      </div>
+      <button className="rounded-2xl px-4 py-1 text-sm flex items-center gap-1 bg-white text-orange-700">
+        Upgrade
+        <FaArrowUp className="text-xs" />
+      </button>
+    </div>
+  );
+};
+
+export default UpgradePremium;
