f0bd961f98d489cc25c35b93133b11275cc184c0
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-10-14 14:59:33 +0300

    install: react-router-dom projeye yüklendi

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="w-full min-h-screen bg-gray-300 flex items-center justify-center flex-col gap-2">
+      <h1 className="text-4xl font-bold">Home</h1>
+      <p className="text-lg">Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default Home;
