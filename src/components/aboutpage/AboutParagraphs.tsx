b49cd41f829de37dfad53161d7271a5facf8028f
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-01-04 21:30:01 +0300

    add: AboutParagraphs içine gsap animasyonları eklendi

--- src/components/aboutpage/AboutParagraphs.tsx
+++ src/components/aboutpage/AboutParagraphs.tsx
@@ -1,12 +1,38 @@
+import { useGSAP } from "@gsap/react";
 import type { AboutProps } from "../../pages/About";
 import { BsStar } from "react-icons/bs";
 import { HiOutlineArrowSmRight } from "react-icons/hi";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const AboutParagraphs = ({ userResponse }: AboutProps) => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".aboutMe",
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: ".aboutMe",
+          start: "top 90%",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
   return (
     <section className="w-full h-[50vh] bg-black p-6 relative px-5 sm:px-10">
       <div className="absolute inset-0 bg-gradient-to-b from-[#0e0e10] to-transparent h-60 z-10"></div>
-      <div className="w-full h-full flex flex-col gap-5 relative z-20">
+      <div className="w-full h-full flex flex-col gap-5 relative z-20 aboutMe">
         <div className="">
           <h2 className="text-6xl">
             Crafting Interfaces <br /> That Feel Alive
