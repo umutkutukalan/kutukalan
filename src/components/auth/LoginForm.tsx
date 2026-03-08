9fe9b2b753504f8adef3361e7bd3d3cc7c5e40e1
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-03-09 00:14:11 +0300

    feat: LoginForm bileşeni güncellendi, gereksiz props kaldırıldı ve stil iyileştirildi

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,18 +2,9 @@ import { BiSolidLock, BiSolidUser } from "react-icons/bi";
 import { useLogin } from "../../hooks/auth/useLogin";
 import { useState } from "react";
 import type { LoginUserData } from "../../services/auth/loginService";
+import { logo2 } from "../../utils";
 
-interface LoginFormProps {
-  setLogin: (value: boolean) => void;
-  setRegister: (value: boolean) => void;
-  setForgotPassword: (value: boolean) => void;
-}
-
-const LoginForm = ({
-  setLogin,
-  setRegister,
-  setForgotPassword,
-}: LoginFormProps) => {
+const LoginForm = () => {
   const { login } = useLogin();
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -30,35 +21,15 @@ const LoginForm = ({
         e.preventDefault();
         login(userData);
       }}
-      className="flex flex-col gap-2 text-gray-800 w-100 bg-gradient-to-t from-gray-200 to-gray-400 p-10 rounded-4xl relative"
+      className="flex flex-col gap-2 text-gray-800 w-100 bg-gradient-to-tr from-gray-800 to-black p-10 rounded-2xl relative"
       style={{
         boxShadow: "2px 2px 10px 0px rgba(0,0,0,0.1)",
       }}
     >
-      <div className="absolute left-1/2 -translate-x-1/2 top-2">
-        <span className="tangerine-regular text-xl">kutukalan</span>
-      </div>
-      <div className="flex flex-col items-center gap-1 mb-5">
-        <h2 className="text-2xl">kutukalan.com | panel</h2>
-        <p className="text-sm opacity-50 text-center">
-          siteye giriş yetkiniz var ise mail ve sifre ile giriş yapabilirsiniz
-        </p>
-      </div>
       <div className="w-full flex flex-col gap-3 text-sm">
-        <div className="w-full grid grid-cols-2 gap-1 text-white rounded-md overflow-hidden text-md">
-          <div className="flex items-center justify-center p-2 bg-gray-500 hover:bg-gray-600 cursor-pointer transition-all">
-            Giris Yap
-          </div>
-          <div
-            className="flex items-center justify-center p-2 bg-gray-500 hover:bg-gray-600 cursor-pointer transition-all"
-            onClick={() => {
-              setLogin(false);
-              setRegister(true);
-            }}
-          >
-            Kullanıcı Olustur
-          </div>
-        </div>
+
+        <img src={logo2} alt="" className="w-40" />
+
         <div className="w-full relative">
           <BiSolidUser
             className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
@@ -67,7 +38,7 @@ const LoginForm = ({
           <input
             type="text"
             placeholder="Username"
-            className="w-full p-3 pl-10 rounded-lg outline-none bg-[#eff3f6]"
+            className="w-full p-3 pl-8 text-sm rounded-lg outline-none bg-[#eff3f6]"
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
@@ -80,25 +51,16 @@ const LoginForm = ({
             <input
               type="password"
               placeholder="Password"
-              className="w-full p-3 pl-10 rounded-lg outline-none bg-[#eff3f6]"
+              className="w-full p-3 pl-8 text-sm rounded-lg outline-none bg-[#eff3f6]"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button
-            className="text-sm opacity-50 cursor-pointer md:opacity-60"
-            onClick={() => {
-              setLogin(false);
-              setForgotPassword(true);
-            }}
-          >
-            Forgot password?
-          </button>
         </div>
         <button
           type="submit"
-          className="w-full bg-[#181922]  p-2 rounded-lg hover:bg-blue-700 transition-colors text-white"
+          className="w-full bg-white/20 p-2 rounded-lg hover:bg-white/30 transition-colors text-white cursor-pointer"
         >
-          Create User
+          Giriş Yap
         </button>
       </div>
     </form>
--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -1,29 +1,10 @@
-import { useState } from "react";
 import LoginForm from "../components/auth/LoginForm";
-import RegisterForm from "../components/auth/RegisterForm";
-import ForgotPassword from "../components/auth/ForgotPassword";
 
 const CreateUser = () => {
-  const [login, setLogin] = useState(true);
-  const [register, setRegister] = useState(false);
-  const [, setForgotPassword] = useState(false);
 
   return (
-    <div className="absolute inset-0 w-full h-full flex items-center justify-center bg-gradient-to-t from-black to-gray-300">
-      {login ? (
-        <LoginForm
-          setLogin={setLogin}
-          setRegister={setRegister}
-          setForgotPassword={setForgotPassword}
-        />
-      ) : register ? (
-        <RegisterForm setLogin={setLogin} setRegister={setRegister} />
-      ) : (
-        <ForgotPassword
-          setLogin={setLogin}
-          setForgotPassword={setForgotPassword}
-        />
-      )}
+    <div className="absolute inset-0 w-full h-full flex items-center justify-center bg-gradient-to-t from-black to-gray-800">
+      <LoginForm />
     </div>
   );
 };
