3aaeff4eecb3d0ae45f1f1b1603fa78b1d2157ce
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-03 00:54:48 +0300

    feat: deleteProjectService fonksiyonuna withCredentials seçeneği eklendi

--- src/services/project/deleteBlogService.ts
+++ src/services/project/deleteBlogService.ts
@@ -3,7 +3,9 @@ import { API_URL } from "../config";
 
 export const deleteProjectService = async (projectId: number): Promise<void> => {
   try {
-    await axios.delete(`${API_URL}/projects/${projectId}`);
+    await axios.delete(`${API_URL}/projects/${projectId}`, {
+      withCredentials: true,
+    });
   } catch {
     throw new Error("Failed to delete project");
   }
