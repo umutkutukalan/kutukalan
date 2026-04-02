14adeddb63ca515f717329d95b0d6ab118567de8
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-04-03 00:53:07 +0300

    feat: deleteBlogService fonksiyonuna withCredentials seçeneği eklendi

--- src/services/blog/deleteBlogService.ts
+++ src/services/blog/deleteBlogService.ts
@@ -3,7 +3,9 @@ import { API_URL } from "../config";
 
 export const deleteBlogService = async (blogId: number): Promise<void> => {
   try {
-    await axios.delete(`${API_URL}/blogs/${blogId}`);
+    await axios.delete(`${API_URL}/blogs/${blogId}`, {
+      withCredentials: true,
+    });
   } catch {
     throw new Error("Failed to delete blog");
   }
